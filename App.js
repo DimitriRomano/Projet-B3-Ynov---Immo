22628006b04a5a5732cb2265d1ff119cd9a98d19
DimitriRomano <dimitri.romano09@gmail.com>
2022-04-05 08:56:00 +0200

    init project

--- App.js
+++ App.js
@@ -1,23 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.mytext}>Open up App.js to start working on your françis</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SplashScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#212120',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  mytext: {
-    color: '#fff'
-  }
-});
+
